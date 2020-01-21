b334eda0e5b96f2b7cdf30b614089091c014cd74
karthikreddyp123 <karthikreddykohli@gmail.com>
2020-01-21 18:29:37 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Singleton DataBaseConnection1 = Singleton.GetInstance;
+            DataBaseConnection1.LogMessage("Connection to Database");
+            Singleton DataBaseConnection2 = Singleton.GetInstance;
+            DataBaseConnection2.LogMessage("Connection to Database");
+            if(DataBaseConnection1== DataBaseConnection2)
+            {
+                Console.WriteLine("Both instances are same.");
+            }
+        }
+    }
+}
---
+++ Singleton.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Singleton
+{
+    sealed class Singleton
+    {
+        static int instanceCounter = 0;
+        private static Singleton instance = null;
+        private Singleton()
+        {
+            instanceCounter++;
+            Console.WriteLine("Instances created " + instanceCounter);
+        }
+        public static Singleton GetInstance
+        {
+            get
+            {
+                
+                    if (instance == null)
+                    {
+                        instance = new Singleton();
+                        Console.WriteLine("Creating New Insatnce.");
+                        return instance;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Using Previous Insatnce.");
+                        return instance;
+                    }
+                
+            }
+        }
+        public void LogMessage(string message)
+        {
+            Console.WriteLine("Message " + message);
+        }
+    }
+}
